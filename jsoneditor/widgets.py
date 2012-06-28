7f89eaa00d9dc4ca4e66dd85444ea9d36b8ffb18
Markus Törnqvist <mjt+illyria@skyhood.com>
2012-06-28 17:13:13 +0300

    Skeletal widget
    from jsoneditor import widgets as jsoneditor_widgets
    
    class JSONesqueForm(forms.ModelForm):
        def __init__(self, *args, **kwargs):
            self.base_fields['json'].widget = jsoneditor_widgets.JSONEditor()
    
            return super(JSONesqueForm, self).__init__(*args, **kwargs)

---
+++ jsoneditor/widgets.py
@@ -0,0 +1,29 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.template.loader import render_to_string
+
+from django.utils.safestring import mark_safe
+
+from django import forms
+
+class JSONEditor(forms.Textarea):
+    class Media:
+        css = {
+            'all': ('css/jsoneditor.css',),
+        }
+        js = ('js/jsoneditor-min.js', 'js/interface.js')
+
+    def render(self, name, value, attrs=None, **kwargs):
+        rendered = super(JSONEditor, self).render(name, value, attrs=attrs, **kwargs)
+
+        context = {
+            'out': 'LOOOOOL 8=O',
+        }
+
+        return rendered +  mark_safe(render_to_string(
+            'jsoneditor/jsoneditor_widget.html', context
+        ))
+
+
+# EOF
+
